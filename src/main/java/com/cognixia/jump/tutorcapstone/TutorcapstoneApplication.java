473a88156e2c7434e763a7a355a4ad3a4169be0a
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-04-05 11:40:40 -0700

    second commit

---
+++ src/main/java/com/cognixia/jump/tutorcapstone/TutorcapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.tutorcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorcapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorcapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognixia/jump/tutorcapstone/TutorcapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.tutorcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorcapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
