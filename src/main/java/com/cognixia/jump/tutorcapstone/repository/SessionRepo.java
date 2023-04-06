86ecd29f93a22a6716bac2e4a1c96104dd8dfabf
Ryan Okamoto <rokam475@gmail.com>
2023-04-06 11:46:36 -0500

    swapped title to tutor for tutor in course. added custom queries to session repo for ratings

--- src/main/java/com/cognixia/jump/tutorcapstone/repository/SessionRepo.java
+++ src/main/java/com/cognixia/jump/tutorcapstone/repository/SessionRepo.java
@@ -2,9 +2,18 @@ package com.cognixia.jump.tutorcapstone.repository;
 
 import com.cognixia.jump.tutorcapstone.model.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SessionRepo extends JpaRepository<Session,Integer> {
+	
+	@Query("SELECT AVG(s.rating) FROM session s WHERE s.course = ?1 GROUP BY s.course")
+	public Double courseAvarage(String course);
+	
+	@Query("SELECT AVG(s.rating) FROM session s LEFT JOIN course c WHERE c.tutor = ?1 GROUP BY c.tutor")
+	public Double tutorAvarage(String tutor);
+	
+	
 
 }
