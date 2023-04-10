504a3ffae906a26fc4139a299c5e0f7573aae675
marco3dog <62681327+marco3dog@users.noreply.github.com>
2023-04-10 08:57:10 -0700

    added JUNIT tests

--- src/test/java/com/cognixia/jump/tutorcapstone/controller/CourseControllerTest.java
+++ src/test/java/com/cognixia/jump/tutorcapstone/controller/CourseControllerTest.java
@@ -5,17 +5,23 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.cognixia.jump.tutorcapstone.model.Course;
@@ -85,5 +91,33 @@ public class CourseControllerTest {
 		
 		verify(service, times(1)).findByUserId(1);
 		verifyNoMoreInteractions(service);
+	}
+
+	@Test
+	void testPostCourse() throws Exception{
+		String uri = STARTING_URI + "/create";
+		Course tempCourse = new Course(1,  new Subject(), new User(), new ArrayList<Session>(), "Now", 12.00);
+		when(service.createCourse(Mockito.any(Course.class))).thenReturn(tempCourse);
+		mvc.perform(post(uri))
+		.andDo(print())
+		.andExpect(status().isCreated());
+	}
+
+	@Test
+	void testUpdateStudent() throws Exception{
+		when(service.updateCourse(Mockito.any(Course.class))).thenReturn(course1);
+		String uri = STARTING_URI + "/update";
+		mvc.perform(put(uri))
+		.andDo(print())
+		.andExpect(status().isOk());
+	}
+
+	@Test
+	void testDeleteStudent() throws Exception{
+		when(service.deleteCourse(course1.getId())).thenReturn(true);
+		String uri = STARTING_URI + "/delete/1";
+		mvc.perform(delete(uri))
+		.andDo(print())
+		.andExpect(status().isOk());
 	}
 }
